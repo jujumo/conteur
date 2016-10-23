c2baf105831350c9cb4f872d2640ca8b0e57fb83
jumo <ju.jumo@gmail.com>
2016-10-23 13:37:23 +0000

    bug fix using tts on pi

--- source/tts.py
+++ source/tts.py
@@ -3,7 +3,8 @@ from os.path import abspath, join, isfile, dirname, isdir
 from subprocess import check_call, Popen
 import tempfile
 import logging
-import urllib
+import urllib.request
+import urllib.parse
 
 
 VOXYGEN_URL_FMT = 'https://www.voxygen.fr/sites/all/modules/voxygen_voices/assets/proxy/index.php?method=redirect&text={message}&voice={voice}'
@@ -22,7 +23,7 @@ def tts_normalize(filepath_in, filepath_out, rate=22050):
         # relies on ffmpeg on windows
         cmd = ['ffmpeg', '-i', filepath_in, '-ar', str(rate), '-y', filepath_out]
     else:
-        cmd = ['sox', filepath_in, filepath_out, 'rate', str(rate)]
+        cmd = ['sox', '-t', 'mp3', filepath_in, filepath_out, 'rate', str(rate)]
 
     success = check_call(cmd)
     if success is not 0:
@@ -41,7 +42,7 @@ def tts_voxygen(filepath, message):
     voice = 'Marion'
     message = urllib.parse.quote(message)
     url = VOXYGEN_URL_FMT.format(message=message, voice=voice)
-    print(url)
+    logging.debug('requesting: {}'.format(url))
     tmp_filepath, headers = urllib.request.urlretrieve(url)
     tts_normalize(tmp_filepath, filepath)
     os.remove(tmp_filepath)
@@ -50,7 +51,10 @@ def tts_voxygen(filepath, message):
 def tts(filepath, message):
     # first try voxygen, and fallback on pico if needed
     try:
+        logging.debug('try to get voice from voxygen')
         tts_voxygen(filepath, message)
-    except:
+    except IOError as e:
+        logging.debug(e)
+        logging.debug('voxygen failed, fallback on pico')
         tts_pico(filepath, message)
 
